bf299d45d6cb757c4fe192db9867eda677159af1
bcXcb <gabriel.lcifba@gmail.com>
2023-07-02 10:32:07 -0300

    alterei o arquivo 1013.java

--- java/1013.java
+++ java/1013.java
@@ -11,7 +11,9 @@ public class Main {
         b = input.nextInt();
         c = input.nextInt();
         
+        // maior entre a e b
         m = ((a + b) + Math.abs(a - b)) / 2;
+        // maior entre maior anterior e c
         m = ((m + c) + Math.abs(m - c)) / 2;
         
         System.out.printf("%d eh o maior\n", m);
