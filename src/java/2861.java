5b92a44be3e59853deb8c6fa3286b3afb9846d21
bcXcb <gabriel.lcifba@gmail.com>
2023-07-10 11:30:46 -0300

    adicionei 2861.java

---
+++ src/java/2861.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n;
+        String ask;
+        Scanner input = new Scanner(System.in);
+        
+        n = input.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            ask = input.next();
+            System.out.println("gzuz");
+        }
+    }
+}
