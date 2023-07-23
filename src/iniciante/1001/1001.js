eb591f69f0bd6db94f6dce1f79e7d724e5022250
bcXcb <gabriel.lcifba@gmail.com>
2023-07-23 16:06:31 -0300

    adicionei 1000.js, 1001.js, 1002.js e 1040.js

---
+++ src/iniciante/1001/1001.js
@@ -0,0 +1,6 @@
+let input = require("fs").readFileSync("/dev/stdin", "utf8");
+let lines = input.split("\n");
+
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+console.log(`X = ${A + B}`);
